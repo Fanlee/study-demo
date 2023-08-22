572993b24ec20efed8bea2b88d12b8205bf180b6
lihuan <anyueleo@163.com>
2023-08-22 10:43:36 +0800

    ✨ feat: 新增双向链表，循环链表

--- linked-list.js
+++ linked-list.js
@@ -2,7 +2,7 @@
  * @Author: lihuan
  * @Date: 2023-08-21 14:30:37
  * @LastEditors: lihuan
- * @LastEditTime: 2023-08-21 16:44:27
+ * @LastEditTime: 2023-08-22 10:40:38
  * @Description: 链表
  */
 
@@ -114,8 +114,131 @@ class LinkedList {
   }
 }
 
-const list = new LinkedList()
-list.push(1)
-list.push(2)
-list.push(3)
-console.log(list.toString())
+class DoublyNode extends Node {
+  constructor(element, next, prev) {
+    super(element, next)
+    this.prev = prev
+  }
+}
+
+class DoublyLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    super(equalsFn)
+    this.tail = undefined
+  }
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new DoublyNode(element)
+      let current = this.head
+      if (index === 0) {
+        if (this.head == null) {
+          this.head = node
+          this.tail = node
+        } else {
+          node.next = this.head
+          current.prev = node
+          this.head = node
+        }
+      } else if (index === this.count) {
+        current = this.tail
+        current.next = node
+        node.prev = current
+        this.tail = node
+      } else {
+        const previous = this.getElementAt(index - 1)
+        current = previous.next
+        node.next = current
+        previous.next = node
+        current.prev = node
+        node.prev = previous
+      }
+      this.count++
+      return true
+    }
+    return false
+  }
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head
+      if (index === 0) {
+        this.head = current.next
+        if (this.count === 1) {
+          this.tail = undefined
+        } else {
+          this.head.prev = undefined
+        }
+      } else if (index === this.count - 1) {
+        current = this.tail
+        this.tail = current.prev
+        this.tail.next = undefined
+      } else {
+        current = this.getElementAt(index)
+        const previous = current.prev
+        previous.next = current.next
+        current.next.prev = previous
+      }
+      this.count--
+      return current.element
+    }
+    return undefined
+  }
+}
+
+class CircularLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    super(equalsFn)
+  }
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element)
+      let current = this.head
+      if (index === 0) {
+        if (this.head == null) {
+          this.head = node
+          node.next = this.head
+        } else {
+          node.next = current
+          current = this.getElementAt(this.size())
+          this.head = node
+          current.next = this.head
+        }
+      } else {
+        const previous = this.getElementAt(index - 1)
+        node.next = previous.next
+        previous.next = node
+      }
+      this.count++
+      return true
+    }
+    return false
+  }
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head
+      if (index === 0) {
+        if (this.size() === 1) {
+          this.head = undefined
+        } else {
+          let removed = this.head
+          current = this.getElementAt(this.size())
+          this.head = this.head.next
+          current.next = this.head
+          current = removed
+        }
+      } else {
+        const previous = this.getElementAt(index - 1)
+        current = previous.next
+        previous.next = current.next
+      }
+      this.count--
+      return current.element
+    }
+    return undefined
+  }
+}
+
+const c = new CircularLinkedList()
+c.insert(1, 0)
+// c.insert(2, 1)
+
+console.log(c)
