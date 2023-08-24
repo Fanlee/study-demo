a23fe0c060067e7d03ecf3db89bb19ea585bfcb5
lihuan <anyueleo@163.com>
2023-08-24 15:36:09 +0800

    ✨ feat: 新增二叉树

--- tree.js
+++ tree.js
@@ -2,7 +2,7 @@
  * @Author: lihuan
  * @Date: 2023-08-23 16:43:51
  * @LastEditors: lihuan
- * @LastEditTime: 2023-08-23 17:10:44
+ * @LastEditTime: 2023-08-24 15:15:42
  * @Description: 树
  */
 import { Compare, defaultCompare } from './util.js'
@@ -43,6 +43,115 @@ export class BinarySearchTree {
       }
     }
   }
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback)
+  }
+  inOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.inOrderTraverseNode(node.left, callback)
+      callback(node.key)
+      this.inOrderTraverseNode(node.right, callback)
+    }
+  }
+  preOrderTraverse(callback) {
+    this.preOrderTraverseNode(this.root, callback)
+  }
+  preOrderTraverseNode(node, callback) {
+    if (node != null) {
+      callback(node.key)
+      this.preOrderTraverseNode(node.left, callback)
+      this.preOrderTraverseNode(node.right, callback)
+    }
+  }
+  postOrderTraverse(callback) {
+    this.postOrderTraverseNode(this.root, callback)
+  }
+  postOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.preOrderTraverseNode(node.left, callback)
+      this.preOrderTraverseNode(node.right, callback)
+      callback()
+    }
+  }
+  min() {
+    return this.minNode(this.root)
+  }
+  minNode(node) {
+    let current = node
+    while (current != null && current.left != null) {
+      current = current.left
+    }
+    return current
+  }
+  max() {
+    return this.maxNode(this.root)
+  }
+  maxNode(node) {
+    let current = node
+    while (current != null && current.right != null) {
+      current = current.right
+    }
+    return current
+  }
+  search(key) {
+    return this.searchNode(this.root, key)
+  }
+  searchNode(node, key) {
+    if (node == null) return false
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      return this.searchNode(node.left, key)
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key)
+    } else {
+      return true
+    }
+  }
+  remove(key) {
+    this.root = this.removeNode(this.root, key)
+  }
+  removeNode(node, key) {
+    if (node == null) {
+      return null
+    }
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      node.left = this.removeNode(node.left, key)
+      return node
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      node.right = this.removeNode(node.right, key)
+      return node
+    } else {
+      if (node.left == null && node.right == null) {
+        node = null
+        return node
+      }
+      if (node.left == null) {
+        node = node.right
+        return node
+      } else if (node.right == null) {
+        node = node.left
+        return node
+      }
+      const aux = this.minNode(node.right)
+      node.key = aux.key
+      node.right = this.removeNode(node.right, aux.key)
+      return node
+    }
+  }
+}
+
+export class AVLTree extends BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    super(compareFn)
+    this.root = null
+  }
+  getNodeHight(node) {
+    if (node == null) {
+      return -1
+    }
+    return (
+      Math.max(this.getNodeHight(node.left), this.getNodeHight(node.right)) + 1
+    )
+  }
 }
 
 const tree = new BinarySearchTree()
@@ -61,4 +170,4 @@ tree.insert(14)
 tree.insert(20)
 tree.insert(18)
 tree.insert(25)
-console.log(tree)
+console.log(tree.search(60))
--- util.js
+++ util.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: lihuan
+ * @Date: 2023-08-21 14:29:37
+ * @LastEditors: lihuan
+ * @LastEditTime: 2023-08-24 15:17:38
+ * @Description:
+ */
 export function defaultEquals(a, b) {
   return a === b
 }
@@ -10,4 +17,12 @@ export const Compare = {
 export function defaultCompare(a, b) {
   if (a === b) return 0
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+export const BalanceFactor = {
+  UNBALANCED_RIGHT: 1,
+  SLIGHTLY_UNBALANCED_RIGHT: 2,
+  BALANCED: 3,
+  SLIGHTLY_UNBALANCED_LEFT: 4,
+  UNBALANCED_LEFT: 5,
 }
